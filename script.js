41e57032ae983eabaa071e2af1f5d3bc603e6583
Garrison Wright <177910142+7-garrison-7@users.noreply.github.com>
2024-09-26 20:11:00 -0400

    Add color to grid squares on hover

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const grid = document.querySelector("#grid");
 
-for (let i = 0; i < 16; i++) {
-    let divRow = document.createElement("div");
-    divRow.setAttribute("class", "divRow");
-    for (let j = 0; j < 16; j++) {
-        let divColumn = document.createElement("div");
-        divColumn.setAttribute("class", "divColumn");
-        divRow.appendChild(divColumn);
+function makeGrid(height, width) {
+
+    for (let i = 0; i < width; i++) {
+        let divRow = document.createElement("div");
+        divRow.setAttribute("class", "divRow");
+
+        for (let j = 0; j < height; j++) {
+            let divColumn = document.createElement("div");
+            divColumn.setAttribute("class", "divColumn");
+            divRow.appendChild(divColumn);
+        }
+
+        grid.appendChild(divRow);
     }
-    grid.appendChild(divRow);
+
 }
 
-// gridContainer.appendChild(divRow);
+makeGrid(16, 16);
+
+const gridSquares = document.querySelectorAll(".divColumn");
+
+gridSquares.forEach((item) => {
+
+    item.addEventListener("mouseenter", () => {
+        item.style.backgroundColor = "red";
+    });
+    
+});
