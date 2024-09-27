673d8e31501b196fae585353bd5eda7cba2d57c9
Garrison Wright <177910142+7-garrison-7@users.noreply.github.com>
2024-09-26 20:26:31 -0400

    Add button to create new grid with user-specified dimensions

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const grid = document.querySelector("#grid");
 
-function makeGrid(height, width) {
+function makeGrid(dimension) {
 
-    for (let i = 0; i < width; i++) {
+    grid.textContent = '';
+    for (let i = 0; i < dimension; i++) {
         let divRow = document.createElement("div");
         divRow.setAttribute("class", "divRow");
 
-        for (let j = 0; j < height; j++) {
+        for (let j = 0; j < dimension; j++) {
             let divColumn = document.createElement("div");
             divColumn.setAttribute("class", "divColumn");
             divRow.appendChild(divColumn);
@@ -15,16 +16,26 @@ function makeGrid(height, width) {
         grid.appendChild(divRow);
     }
 
+    const gridSquares = document.querySelectorAll(".divColumn");
+
+    gridSquares.forEach((item) => {
+    
+        item.addEventListener("mouseenter", () => {
+            item.style.backgroundColor = "red";
+        });
+    
+    });
 }
 
-makeGrid(16, 16);
+makeGrid(16);
 
-const gridSquares = document.querySelectorAll(".divColumn");
+function makeNewGrid () {
+    const dimension = prompt("Enter number of squares per side (max. 100): ");
 
-gridSquares.forEach((item) => {
-
-    item.addEventListener("mouseenter", () => {
-        item.style.backgroundColor = "red";
-    });
+    if (dimension < 100) {
+        makeGrid(dimension);
+    } else {
+        alert("Dimension must be less than 100!");
+    }
     
-});
+}
